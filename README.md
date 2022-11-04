97c23211ba92a7a683cea0c6558eeb3f2b0c08cf
nathanhawk2 <ntrumpet620@gmail.com>
2022-11-04 18:13:22 -0400

    did readme and got post to work

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# tech-blog
+# Tech Blog
+
+## Description
+When you open the site, you can log in.
+
+## Visuals
+![login website](./login.png)
+This is what is seen when you go to login as a user.
+
+![get posts code](./post.png)
+This snippet of code gets all the posts.
+
+## Installation
+Run NPM I once you clone it over to your local computer then type in the CLI npm run start
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,20 +6,35 @@ const withAuth = require('../utils/auth');
 // Prevent non logged in users from viewing the homepage
 router.get('/', async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+    // const userData = await User.findAll({
+    //   attributes: { exclude: ['password'] },
+    //   order: [['name', 'ASC']],
+    // });
+
+    // const users = userData.map((project) => project.get({ plain: true }));
+
+    const postData = await Post.findAll({
+      
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      
     });
 
-    const users = userData.map((project) => project.get({ plain: true }));
+    const posts = postData.map((post) => post.get({ plain: true }));
 
     res.render('homepage', {
-      users,
+      posts,
+      // users,
       // Pass the logged in flag to the template
       logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
+    console.log(err)
   }
 });
 
